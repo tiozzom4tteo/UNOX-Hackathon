b7c0322519cc8f8074cc9b321671ae9501cb0b76
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2024-03-09 14:01:55 +0100

    test css

--- chatbot/chatbot_app.py
+++ chatbot/chatbot_app.py
@@ -1,39 +1,51 @@
-import streamlit as st #all streamlit commands will be available through the "st" alias
-import chatbot_lib as glib #reference to local lib script
+import streamlit as st  # all streamlit commands will be available through the "st" alias
+import chatbot_lib as glib  # reference to local lib script
 
 
-st.set_page_config(page_title="Chatbot") #HTML title
-st.title("Chatbot") #page title
+def load_css(file_name):
+    with open(file_name, "r") as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
-if 'memory' not in st.session_state: #see if the memory hasn't been created yet
-    st.session_state.memory = glib.get_memory() #initialize the memory
+st.set_page_config(page_title="Ace")  # HTML title
+st.title("Ace - Your Everyday Assistant")  # page title
 
 
-if 'chat_history' not in st.session_state: #see if the chat history hasn't been created yet
-    st.session_state.chat_history = [] #initialize the chat history
+if 'memory' not in st.session_state:  # see if the memory hasn't been created yet
+    st.session_state.memory = glib.get_memory()  # initialize the memory
 
 
-#Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
-for message in st.session_state.chat_history: #loop through the chat history
-    with st.chat_message(message["role"]): #renders a chat line for the given role, containing everything in the with block
-        st.markdown(message["text"]) #display the chat content
+if 'chat_history' not in st.session_state:  # see if the chat history hasn't been created yet
+    st.session_state.chat_history = []  # initialize the chat history
 
 
+# Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
+for message in st.session_state.chat_history:  # loop through the chat history
+    # renders a chat line for the given role, containing everything in the with block
+    with st.chat_message(message["role"]):
+        st.markdown(message["text"])  # display the chat content
 
-input_text = st.chat_input("Chat with your bot here") #display a chat input box
 
-if input_text: #run the code in this if block after the user submits a chat message
-    
-    with st.chat_message("user"): #display a user chat message
-        st.markdown(input_text) #renders the user's latest message
-    
-    st.session_state.chat_history.append({"role":"user", "text":input_text}) #append the user's latest message to the chat history
-    
-    chat_response = glib.get_chat_response(input_text=input_text, memory=st.session_state.memory) #call the model through the supporting library
-    
-    with st.chat_message("assistant"): #display a bot chat message
-        st.markdown(chat_response) #display bot's latest response
-    
-    st.session_state.chat_history.append({"role":"assistant", "text":chat_response}) #append the bot's latest message to the chat history
-    
+# display a chat input box
+input_text = st.chat_input("Ask Ace...")
+
+if input_text:  # run the code in this if block after the user submits a chat message
+
+    with st.chat_message("user"):  # display a user chat message
+        st.markdown(input_text)  # renders the user's latest message
+
+    # append the user's latest message to the chat history
+    st.session_state.chat_history.append({"role": "user", "text": input_text})
+
+    # call the model through the supporting library
+    chat_response = glib.get_chat_response(
+        input_text=input_text, memory=st.session_state.memory)
+
+    with st.chat_message("assistant"):  # display a bot chat message
+        st.markdown(chat_response)  # display bot's latest response
+
+    # append the bot's latest message to the chat history
+    st.session_state.chat_history.append(
+        {"role": "assistant", "text": chat_response})
+
+load_css("styles.css")
