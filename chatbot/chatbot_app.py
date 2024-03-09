1d715df0c8d3e8211d89fcf63c9b2545c248416d
NickP3lle <53746529+NickP3lle@users.noreply.github.com>
2024-03-09 14:22:48 +0100

    Fixed double code

--- chatbot/chatbot_app.py
+++ chatbot/chatbot_app.py
@@ -9,6 +9,7 @@ def load_css(file_name):
 
 
 st.set_page_config(page_title="Ace")  # HTML title
+load_css("styles.css")
 st.title("Ace - Your Everyday Assistant")  # page title
 
 
@@ -20,15 +21,8 @@ if 'memory' not in st.session_state:  # see if the memory hasn't been created ye
 
 if 'chat_history' not in st.session_state:  # see if the chat history hasn't been created yet
     st.session_state.chat_history = []  # initialize the chat history
-if 'chat_history' not in st.session_state:  # see if the chat history hasn't been created yet
-    st.session_state.chat_history = []  # initialize the chat history
 
 
-# Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
-for message in st.session_state.chat_history:  # loop through the chat history
-    # renders a chat line for the given role, containing everything in the with block
-    with st.chat_message(message["role"]):
-        st.markdown(message["text"])  # display the chat content
 # Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
 for message in st.session_state.chat_history:  # loop through the chat history
     # renders a chat line for the given role, containing everything in the with block
@@ -47,8 +41,7 @@ if input_text:  # run the code in this if block after the user submits a chat me
     # append the user's latest message to the chat history
     st.session_state.chat_history.append({"role": "user", "text": input_text})
 
-    # call the model through the supporting library
-    # chat_response = glib.get_chat_response(input_text=input_text, memory=st.session_state.memory)
+    # use an empty container for streaming output
     st_callback = StreamlitCallbackHandler(st.container())
     chat_response = glib.get_chat_response(
         prompt=input_text, memory=st.session_state.memory, streaming_callback=st_callback)
@@ -59,23 +52,3 @@ if input_text:  # run the code in this if block after the user submits a chat me
     # append the bot's latest message to the chat history
     st.session_state.chat_history.append(
         {"role": "assistant", "text": chat_response})
-if input_text:  # run the code in this if block after the user submits a chat message
-
-    with st.chat_message("user"):  # display a user chat message
-        st.markdown(input_text)  # renders the user's latest message
-
-    # append the user's latest message to the chat history
-    st.session_state.chat_history.append({"role": "user", "text": input_text})
-
-    # call the model through the supporting library
-    chat_response = glib.get_chat_response(
-        input_text=input_text, memory=st.session_state.memory)
-
-    with st.chat_message("assistant"):  # display a bot chat message
-        st.markdown(chat_response)  # display bot's latest response
-
-    # append the bot's latest message to the chat history
-    st.session_state.chat_history.append(
-        {"role": "assistant", "text": chat_response})
-
-load_css("styles.css")
